378ed2c567cb1bb791cc0398207d612c848a59a4
medranojose <67928743+medranojose@users.noreply.github.com>
2020-07-07 21:12:11 -0600

    comentario en MainActivity.java

--- app/src/main/java/com/jl/holamundo/MainActivity.java
+++ app/src/main/java/com/jl/holamundo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // este es un mensaje de prueba para hacer commit
     }
 }
